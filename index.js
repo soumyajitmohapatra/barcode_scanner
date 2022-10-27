e9122b0972cd263beabb9b3ac8c04b939ea64676
soumyajitmohapatra <hellosoumyajit@gmail.com>
2022-10-27 14:02:37 +0530

    style changes

--- index.js
+++ index.js
@@ -98,6 +98,27 @@ function step(capturer) {
   });
 }
 
+var resultContainer = document.getElementById("qr-reader-results");
+var lastResult,
+  countResults = 0;
+
+function onScanSuccess(decodedText, decodedResult) {
+  if (decodedText !== lastResult) {
+    ++countResults;
+    lastResult = decodedText;
+    // Handle on success condition with the decoded message.
+    console.log(`Scan result ${decodedText}`, decodedResult);
+  }
+  ele.id == decodedText ? openModal(ele) : alert("no user found");
+  step(capturer);
+}
+
+var html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", {
+  fps: 10,
+  qrbox: 250,
+});
+html5QrcodeScanner.render(onScanSuccess);
+
 const userData = [
   {
     id: 53790547,
